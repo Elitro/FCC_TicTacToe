6458cdd5732c5da8cf117cc1183cb7f838dcb463
elitro <elitruh@gmail.com>
2017-03-18 01:14:12 +0000

    options menu finalized, working on the game

--- src/app/choose-piece/choose-piece.component.ts
+++ src/app/choose-piece/choose-piece.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-choose-piece',
@@ -10,7 +11,7 @@ export class ChoosePieceComponent implements OnInit {
 
   optionsForm: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private router: Router) { }
 
   ngOnInit() {
 
@@ -21,7 +22,16 @@ export class ChoosePieceComponent implements OnInit {
 
   }
 
+  toggleMarker(value: string) {
+    this.optionsForm.controls['marker'].setValue(value);
+  }
+
+  togglePlayers(value: string) {
+    this.optionsForm.controls['players'].setValue(value);
+  }
+
   submitOptions(form: FormGroup) {
+    this.router.navigate(['game']);
     console.log('submit!', form);
   }
 
